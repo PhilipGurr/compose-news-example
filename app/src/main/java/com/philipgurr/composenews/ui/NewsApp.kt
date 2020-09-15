0105129f3e8dfdfa7845e6bef192ae92c7c03e39
Philip Gurr <philipgurr10@gmail.com>
2020-09-15 21:57:21 +0200

    Add navigation animation

--- app/src/main/java/com/philipgurr/composenews/ui/NewsApp.kt
+++ app/src/main/java/com/philipgurr/composenews/ui/NewsApp.kt
@@ -1,5 +1,7 @@
 package com.philipgurr.composenews.ui
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -14,14 +16,16 @@ import com.philipgurr.composenews.viewmodel.Screen
 fun NewsApp(navigationViewModel: NavigationViewModel, appContainer: AppContainer) {
     val currentScreen by navigationViewModel.currentScreen.observeAsState()
 
-    when(currentScreen) {
-        is Screen.NewsDetail -> NewsDetailScreen(navigationViewModel, appContainer.newsRepository, (currentScreen as Screen.NewsDetail).newsPost)
-        is Screen.FavoritesList -> FavoritesListScreen(navigationViewModel, appContainer.newsRepository) {
-            navigationViewModel.navigateTo(it)
-        }
-        else -> {
-            NewsListScreen(navigationViewModel, appContainer.newsRepository) {
+    Crossfade(current = currentScreen, animation = tween(500)) { screen ->
+        when(screen) {
+            is Screen.NewsDetail -> NewsDetailScreen(navigationViewModel, appContainer.newsRepository, screen.newsPost)
+            is Screen.FavoritesList -> FavoritesListScreen(navigationViewModel, appContainer.newsRepository) {
                 navigationViewModel.navigateTo(it)
+            }
+            else -> {
+                NewsListScreen(navigationViewModel, appContainer.newsRepository) {
+                    navigationViewModel.navigateTo(it)
+                }
             }
         }
     }
